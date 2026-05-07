b8a918a6a1b2314a7e92835b05d3a218d12bf54b
florinbaciuu <baciuaurelflorin@gmail.com>
2026-05-07 15:21:28 +0300

    first commit

---
+++ frameworks/bsp_lvgl_framework/include/lvgl_framework.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*******************************
+ *   LVGL FUNCTIONS PROTOTYPES
+ ******************************/
+
+/**
+ * This function initializes the LVGL kernel and starts the necessary FreeRTOS tasks for LVGL to operate.
+ * It ensures that the LVGL main task and, if configured, the tick task are created and pinned to the appropriate core.
+ * The function also includes a delay to allow the tasks to start properly before returning.
+ * It uses a static variable to ensure that the initialization code is only executed once, preventing multiple initializations 
+ * if the function is called multiple times.    
+ */
+void lvgl_kernel_start(void);
+
+// -------------------------------
+
+
+void lvgl_execute_locked(void (*func)(void));
+void s_lvgl_input_device_config() ;
+bool s_lvgl_port_init_locking_mutex(void);
+bool s_lvgl_lock(TickType_t timeout);
+void s_lvgl_unlock(void);
+void lvgl_framework_init(void);
+
+#define LVGL_LOCK()   s_lvgl_lock(portMAX_DELAY)
+#define LVGL_UNLOCK() s_lvgl_unlock()
+
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ interfaces/bsp_touch_interface/touch_bsp_interface.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#include <stdbool.h>
+#include <stdint.h>
+
+void bsp_touchscreen_init(void);
+
+bool touch_read(uint16_t* x, uint16_t* y);
